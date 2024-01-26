4f4acf47b1e7acd616613906ad29acf4eef5cb9f
Jon Shipman <jon@jonshipman.com>
2024-01-25 19:27:20 -0600

    simple redirect to amazon wish list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
+
+	const DESTINATION =
+		'https://www.amazon.com/hz/wishlist/ls/3PBM618DVH2V5?ref_=list_d_wl_lfu_nav_7';
+
+	$: url = $page.url.toString();
+</script>
+
+<svelte:head>
+	<title>HazelShipman.com</title>
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Hazel Shipman" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="{url}favicon.png" />
+
+	{#if !dev}
+		<meta http-equiv="refresh" content="1; url={DESTINATION}" />
+	{/if}
+</svelte:head>
