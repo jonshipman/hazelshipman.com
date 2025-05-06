6b972984bd51266b11d9e4f03fe7f5233b300abc
Jon Shipman <jon@jonshipman.com>
2025-05-06 16:17:44 +0000

    remove environment check and simplify redirect to Redbubble shop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import { dev } from '$app/environment';
 	import { page } from '$app/stores';
 
-	let DESTINATION =
-		'https://www.amazon.com/hz/wishlist/ls/3PBM618DVH2V5?ref_=list_d_wl_lfu_nav_7';
-	DESTINATION = 'https://www.redbubble.com/people/jonshipman/shop?artistUserName=jonshipman&asc=u&collections=4245161';
+	let DESTINATION = 'https://www.redbubble.com/people/jonshipman/shop?artistUserName=jonshipman&asc=u&collections=4245161';
 
 	$: url = $page.url.toString();
 </script>
@@ -17,7 +14,5 @@
 	<meta property="og:description" content="" />
 	<meta property="og:image" content="{url}favicon.png" />
 
-	{#if !dev}
-		<meta http-equiv="refresh" content="1; url={DESTINATION}" />
-	{/if}
+	<meta http-equiv="refresh" content="1; url={DESTINATION}" />
 </svelte:head>
